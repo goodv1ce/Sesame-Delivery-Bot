bc22ba84078f577c2608bfd6165c0826323e73a9
goodv1ce <andriy.lysak.2003@gmail.com>
2024-06-17 09:13:04 +0300

    [INIT] init commit

---
+++ src/main/java/ua/sesame/delivery_bot/DeliveryBotApplication.java
@@ -0,0 +1,13 @@
+package ua.sesame.delivery_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/sesame/delivery_bot/DeliveryBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.sesame.delivery_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
